4b998189f78318a84704551559490c9d69753fa1
voidKandy <voidkandy@gmail.com>
2025-11-05 15:05:06 -0800

    improved tests

--- tests/all.zig
+++ tests/all.zig
@@ -1,20 +1,75 @@
 const std = @import("std");
 const zemplate = @import("zemplate");
+const Lexer = zemplate.parse.Lexer;
+const TokenType = zemplate.parse.TokenType;
+const Token = zemplate.parse.Token;
 
-const Test = struct { field: []const u8, attr: []const u8 };
-const TestTemplate = zemplate.template.Template(Test, @embedFile("test.html"));
-// test file contains:
-// <div>
-//  ||zz .field zz||
-//  <div attribute="||zz .attr zz||"></div>
+test "readme test" {
+    const allocator = std.testing.allocator;
+    const MyContext = struct { field: []const u8 };
+
+    const MyTemplate = zemplate.Template(MyContext,
+        \\ Hello ||zz .field zz||!
+    );
+
+    var tmplt = MyTemplate.init(MyContext{ .field = "World" }, allocator);
+    const expected =
+        \\ Hello World!
+    ;
+    var render = try tmplt.render();
+    defer render.deinit(allocator);
+
+    if (!std.mem.eql(u8, expected, render.items)) {
+        std.log.err(
+            \\ did not get expected render!
+            \\ Expected:
+            \\ {s}
+            \\ got:
+            \\ {s}
+            \\
+        , .{ expected, render.items });
+        return;
+    }
+    std.debug.print("README Test PASSED\n", .{});
+}
 
 test "render test" {
+    const Test = struct {
+        field1: []const u8,
+        field2: []u8,
+        field3: std.ArrayList(u8),
+
+        fn deinit(self: *@This(), a: std.mem.Allocator) void {
+            a.free(self.field2);
+            self.field3.deinit(a);
+        }
+    };
+
+    // test file contains:
+    // <div>
+    //    ||zz .field1 zz||
+    //    <div style="||zz .field2 zz||">
+    //    ||zz .field3 zz||
+    //   </div>
+    // </div>
+    const TestTemplate = zemplate.Template(Test, @embedFile("test.html"));
     const allocator = std.testing.allocator;
-    var template = try TestTemplate.init(Test{ .field = "mom", .attr = "this-attr" }, allocator);
+    var ctx = Test{
+        .field1 = "this is a field",
+        .field2 = try allocator.dupe(u8, "this is field 2"),
+        .field3 = std.ArrayList(u8).fromOwnedSlice(try allocator.dupe(u8, "this is field 3")),
+    };
+
+    defer ctx.deinit(allocator);
+
+    var template = TestTemplate.init(ctx, allocator);
     const expected =
         \\<div>
-        \\  mom
-        \\  <div attribute="this-attr"></div>
+        \\  this is a field
+        \\  <div style="this is field 2">
+        \\    this is field 3
+        \\  </div>
+        \\</div>
     ;
 
     var render = try template.render();
@@ -35,9 +90,6 @@ test "render test" {
     std.debug.print("Render Test PASSED\n", .{});
 }
 
-const Lexer = zemplate.template.parse.Lexer;
-const TokenType = zemplate.template.parse.TokenType;
-const Token = zemplate.template.parse.Token;
 test "lexing test" {
     const allocator = std.testing.allocator;
     var arena = std.heap.ArenaAllocator.init(allocator);
