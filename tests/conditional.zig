7a5b6a2f1b7d45e33512ff97a89c7ca9dae6103f
voidKandy <voidkandy@gmail.com>
2025-12-30 13:11:46 -0800

    moved Failure struct to shared.zig

--- tests/conditional.zig
+++ tests/conditional.zig
@@ -4,6 +4,7 @@ const print = std.debug.print;
 const panic = std.debug.panic;
 const shared = @import("shared.zig");
 const runTest = shared.runTest;
+const Failure = shared.Failure;
 
 test "conditional" {
     std.testing.log_level = .warn;
@@ -122,81 +123,3 @@ fn ConditionalTestCase(comptime TemplateContext: type) type {
         }
     };
 }
-
-const Failure = struct {
-    index: usize,
-    start: usize,
-    exp_end: usize,
-    got_end: usize,
-    exp_ctx: []const u8,
-    got_ctx: []const u8,
-    expected: []u8,
-    got: []u8,
-
-    const CONTEXT_SIZE = 10;
-    pub fn format(self: @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
-        try writer.print(
-            \\ Render mismatch at index {d}
-        , .{self.index});
-        try writer.print(
-            \\ Expected context ({}..{}): "{s}"
-        , .{
-            self.start,
-            self.exp_end,
-            self.exp_ctx,
-        });
-        try writer.print(
-            \\ Actual   context ({}..{}): "{s}"
-        , .{
-            self.start,
-            self.got_end,
-            self.got_ctx,
-        });
-        if (self.index < self.expected.len)
-            try writer.print(
-                \\ Expected char: '{c}' (byte {d})
-            , .{
-                self.expected[self.index],
-                self.expected[self.index],
-            })
-        else
-            try writer.writeAll("Given index is too large for expected content\n");
-
-        if (self.index < self.got.len)
-            try writer.print(
-                \\ Actual   char: '{c}' (byte {d})
-            , .{
-                self.got[self.index],
-                self.got[self.index],
-            })
-        else
-            try writer.writeAll("Given index is too large for got content\n");
-    }
-
-    fn checkForFailure(a: std.mem.Allocator, got: []const u8, expected: []const u8) ?Failure {
-        const idx = std.mem.indexOfDiff(u8, expected, got) orelse return null;
-        const start = @max(idx, @as(usize, Failure.CONTEXT_SIZE)) - Failure.CONTEXT_SIZE;
-        const end_expected = @min(expected.len, idx + Failure.CONTEXT_SIZE);
-        const end_actual = @min(got.len, idx + Failure.CONTEXT_SIZE);
-        const my_got = a.dupe(u8, got) catch @panic("out of memory");
-        const my_expected = a.dupe(u8, expected) catch @panic("out of memory");
-
-        const exp_ctx = my_expected[start..end_expected];
-        const act_ctx = my_got[start..end_actual];
-        return .{
-            .index = idx,
-            .start = start,
-            .exp_end = end_expected,
-            .got_end = end_actual,
-            .exp_ctx = exp_ctx,
-            .got_ctx = act_ctx,
-            .expected = my_expected,
-            .got = my_got,
-        };
-    }
-
-    fn deinit(self: *@This(), a: std.mem.Allocator) void {
-        a.free(self.got);
-        a.free(self.expected);
-    }
-};
